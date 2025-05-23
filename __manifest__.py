725363ba4f9d27272afb2e150bcbc3c91b1a774d
LariFeuzeu <151120984+LariFeuzeu@users.noreply.github.com>
2025-05-23 12:36:32 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -22,9 +22,9 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+         'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/templates.xml',
         'views/SaleOrder.xml'
     ],
     # only loaded in demonstration mode
